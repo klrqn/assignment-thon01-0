7a51c7800786ae3245387d8ce2d719d0bcbce8f0
klrqn <burtonsb@gmail.com>
2018-04-28 15:50:20 +0000

    dealership.js added

---
+++ dealership.js
@@ -0,0 +1,19 @@
+var myHonda = {color: 'red', wheels: 4, engine: {cylinders: 4, size: 2.2}};
+
+var dealership = {
+
+}
+
+// Assignment-thon 1.0
+// Assignment-thon Version 1.0 (30 minutes)
+
+// Javascript object for a car dealership
+
+// 3 car categories: sedan, suv, sports
+// rental price for each
+// available or not
+
+// total num available for each category
+// ability to submit a rental request
+
+// development done locally, or in c9, testing in browser console or c9 console only. Push to github. Branch “assignment-thon01-0”
